b79dfebc8e0eaf2e00ec10a725b6f3248c9f9a3c
“Ebram_Samuel” <ebramsam2001@gmail.com>
2026-05-19 18:05:33 +0300

    add backend and set up the environment of it

---
+++ backend/app.js
@@ -0,0 +1,32 @@
+// Dotenv
+require("dotenv").config();
+
+// express
+const express = require("express");
+const app = express();
+
+// middleware json
+app.use(express.json());
+
+// connectio DB
+
+// simple logger
+if(process.env.NODE_ENV === "dev") {
+    app.use((req, res, next) => {
+        console.log(`${req.method} ${req.originalUrl}`);
+        next();
+    });
+}
+
+// Test rout
+app.get("/test", (req, res, next) => {
+    return res.status(200).json({message: "Test Route"});
+});
+
+// Port
+const PORT = process.env.PORT || 8000;
+
+// Run server
+app.listen(PORT, () => {
+    console.log(`Server is running in: ${PORT}`);
+});
